98111ff2a3f1ab7381fe25d42b376b4826a7fbab
axis <axisotherwise@gmail.com>
2022-08-05 17:51:46 +0900

    Initial settings

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-"# SimpleBlog" 
+# SimpleBlog
+
---
+++ src/config/config.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config = {
+  "development": {
+    "username": "root",
+    "password": "broccoli",
+    "database": "simpleblog",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "logging": true,
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+};
+
+export default config;
