4a9b0dd889a544df66e1cf30c826fc76ce8017bf
jinguang <chjg1231@gmail.com>
2022-08-10 17:55:19 +0900

    post 컨트롤 얼추 만들기

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,7 +1,6 @@
-import express from "express";
-
-import User from "../models/user.js"
+import User from "../models/user.js";
 import Post from "../models/post.js";
+import Comment from "../models/comment.js";
 
 const postcreate = async (req, res, next) => {//게시글 생성 API
   try{
@@ -11,11 +10,12 @@ const postcreate = async (req, res, next) => {//게시글 생성 API
     await Post.create({ userId, title, content });
 
     return res.status(201).json({ Message: "게시글 작성에 성공했습니다." });
-  }catch (error){
+  }catch (err){
     console.error(err, { Message: "게시글 작성에 실패했습니다." } );
     next(err);
   };    
 };
+
 const postLooks = async (req, res, next) => { // 전체 조회
   try{
     const posts = await Post.findAll({
@@ -36,12 +36,18 @@ const postLooks = async (req, res, next) => { // 전체 조회
 const postLook = async (req, res, next) => { // 상세 조회
   try{
     const { postId } = req.params;
-    const posts = await Post.findOne({ where: { id: postId } });
+    const posts = await Post.findOne({ where: { id: postId }, 
+      include: {
+        model: User,
+        attributes: ["name"],
+      },
+    });
+    const comment = await Comment.findAll({ where: { postId }})
 
     if(!posts){
       return res.status(400).json({ errorMessage: "게시글이 없습니다."});
     }else{
-      return res.status(200).json({ posts });
+      return res.status(200).json({ posts, comment });
     };
   }catch(err){
     console.error(err, { Message: "게시글 상세 조회에 실패했습니다." } );
@@ -51,30 +57,36 @@ const postLook = async (req, res, next) => { // 상세 조회
 };
 
 const postupdate = async (req, res, next) => {
-    try{
-        const { postId } = req.params;
-        const { title, content } = req.body;
-    
-        await Post.update({ title, content }, { where: { postId }});
-      next()
-    }catch(error){
-      console.error(err, { Message: "게시글 수정 실패 " } );
-      next(err);
-    }
-  
-  };
+  try{
+    const { postId } = req.params;
+    const { title, content } = req.body;
+    const post = await Post.findOne({ where: { id: postId } });
+    if(!post){
+        return res.status(400).json({ errorMessage: "게시글이 없습니다."});
+      }else{
+        await Post.update({ title, content }, { where: { id: postId }});
+        return res.status(200).json({ Message: "게시글이 수정 되었습니다."});
+      };
+  }catch(err){
+    console.error(err, { Message: "게시글 수정 실패 " } );
+    next(err);
+  };  
+};
 
 const portdelete = async (req, res, next) => {
   try{
     const { postId } = req.params;
-
-    const post = await Post.findOne({ where: { postId } })
-
-    await Post.status(200).destroy({ where: { postId } })
-  }catch(error){
-    console.error(err, { Message: "게시글 조회에 실패했습니다." } );
+    const post = await Post.findOne({ where: { id: postId } });
+    if(!post){
+        return res.status(400).json({ errorMessage: "게시글이 없습니다."});
+      }else{
+        await Post.destroy({ where: { id: postId } });
+        return res.status(200).json({ Message: "게시글이 삭제 되었습니다."});
+      };
+  }catch(err){
+    console.error(err, { Message: "게시글 삭제에 실패했습니다." } );
     next(err);
-  }
+  };
 };
 
 export {
