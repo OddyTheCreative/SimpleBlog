98ec425591103c8148763844ff6883398233a97e
axis <axisotherwise@gmail.com>
2022-08-10 16:08:32 +0900

    16:08

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 
 import * as postControllers from "../controllers/post.js";
@@ -14,11 +13,5 @@ router.get("/:postId",postControllers.postLook);
 router.put("/:postId",postControllers.postupdate);
 
 router.delete("/:postId",postControllers.portdelete);
-=======
-import * as postController from "../controllers/posts.js";
-const router = express.Router();
-
-router.post("/post", postController.auth, postController.createPost);
->>>>>>> aa36369349578493d0708ff3fd7959f41b7b77b4
 
 export default router;
